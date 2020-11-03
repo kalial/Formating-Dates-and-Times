b1d453a52e440da20a092bc6e9132ea7ff7f3251
Kali <al-ammari@mediamarktsaturn.com>
2020-11-03 18:51:24 +0100

    Done

--- src/NiceDates.java
+++ src/NiceDates.java
@@ -1,5 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class NiceDates {
     public static void main(String[] args){
+        DateTimeFormatter f1 = DateTimeFormatter.ofPattern("MMM dd, yyyy");
+        DateTimeFormatter f2 = DateTimeFormatter.ofPattern("E MMM dd, yyyy G");
+        DateTimeFormatter tf1 = DateTimeFormatter.ofPattern("k:m:s A a");
+
+        LocalDate d = LocalDate.now();
+        String s = d.format(f1);
+
+        System.out.println(s);
+        System.out.println(d.format(f2));
+
+        LocalTime t = LocalTime.now();
+        System.out.println(t.format(tf1));
 
     }
 }
